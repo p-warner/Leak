04b62ed3f94f5533a1a46e398f84161615624cf4
Phil <pwarner@gmail.com>
2024-01-25 11:53:56 -0500

    Add SQL connector.

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+	
+	private final static String uname = "service_acct";
+	private final static String pass = "asldhas9s";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Gather infi from user.");
+		
+		System.out.println("INsert into database.");
+		
+		System.out.println("Connect to DB with " + uname + ":" + pass);
+	}
+
+}
